b9519902d45b8bc14069eb1facdbcc80919a4310
tavo <gasb_150@hotmail.com>
2021-02-02 12:24:57 -0500

    fix new post form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,4 +9,36 @@ class PostsController < ApplicationController
     def new
         @post = Post.new
     end
+
+    def create
+        @post = Post.new(post_params)
+    
+        respond_to do |format|
+          if @post.save
+            format.html { redirect_to root_path, notice: 'Post was successfully created.' }
+            format.json { render :show, status: :created, location: @post }
+          else
+            format.html { render :new, status: :unprocessable_entity }
+            format.json { render json: @post.errors, status: :unprocessable_entity }
+          end
+        end
+    end
+
+
+    # def create
+    #     #@post = Post.new(posts_params)
+    #     @post = Post.new(:title => params[:title], :body => params[:body]) 
+    #     if @post.save
+    #     redirect_to new_post_path
+    #     else
+    #     render :new
+    #     end 
+    # end
+
+    private
+
+    def post_params
+        params.require(:post).permit(:body, :title)
+    end 
+
 end
